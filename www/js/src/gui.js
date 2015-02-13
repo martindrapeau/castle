34027e8ed2975beec1ef9c089bb902c74585a75a
Martin Drapeau <martindrapeau@gmail.com>
2015-02-13 16:22:49 -0500

    Added panel for credits, high scores, etc. Just need to fill them in now...

--- www/js/src/gui.js
+++ www/js/src/gui.js
@@ -20,7 +20,7 @@
     })
   });
 
-  Backbone.HighScore = Backbone.Button.extend({
+  Backbone.SavedGame = Backbone.Button.extend({
     defaults: _.extend({}, Backbone.Button.prototype.defaults, {
       x: 960,
       y: 400,
@@ -62,6 +62,53 @@
       context.fillText(time, x+80, y+170);
     }
   });
+
+  Backbone.Panel = Backbone.Button.extend({
+    defaults: _.extend({}, Backbone.Button.prototype.defaults, {
+      x: 160,
+      y: 720,
+      width: 640,
+      height: 140,
+      backgroundColor: "transparent",
+      img: "#artifacts", imgX: 0, imgY: 1010, imgWidth: 640, imgHeight: 480, imgMargin: 0,
+      text: "",
+      textPadding: 24,
+      textContextAttributes: {
+        fillStyle: "#F67D00",
+        font: "34px arcade, Verdana, Arial, Sans-Serif",
+        textBaseline: "middle",
+        fontWeight: "normal",
+        textAlign: "center"
+      },
+      easing: "easeOutCubic",
+      easingTime: 600
+    })
+  });
+
+  Backbone.Credits = Backbone.Panel.extend({
+    defaults: _.extend({}, Backbone.Panel.prototype.defaults, {
+      text: "Credits"
+    }),
+    draw: function(context) {
+      Backbone.Panel.prototype.draw.apply(this, arguments);
+      var x = this.get("x"),
+          y = this.get("y");
+
+    }
+  })
+
+  /*
+    --Credits--
+
+    Graphics: pzUH, TikusJenaka, kemotaku, simirk, dxc, Saranai
+    Music: Edvard Grieg
+    Testing: Lodovic, Emilia
+    Coding: Martin
+
+    Built with Backbone Game Engine
+
+    A Game by Martin Drapeau
+  */
 
 	Backbone.Gui = Backbone.Model.extend({
     defaults: {
@@ -112,44 +159,47 @@
       });
 
       this.resume = new Backbone.PullOutButton({
-        y: 400,
+        y: 420,
         text: "Resume "
       });
       this.resume.on("tap", function() {
         gui.trigger("resume");
       });
 
-      this.highScore = new Backbone.HighScore({}, {
+      this.showCredits = new Backbone.PullOutButton({
+        y: 540,
+        text: "Credits "
+      });
+
+      this.savedGame = new Backbone.SavedGame({}, {
         world: this.world
       });
+
+      this.credits = new Backbone.Credits();
+      this.showCredits.on("tap", _.partial(this.showPanel, this.credits), this);
 
       this.on("attach", this.onAttach);
       this.on("detach", this.onDetach);
     },
+    spawnImg: Backbone.SpriteSheet.prototype.spawnImg,
     onAttach: function() {
       this.onDetach();
 
-      this.engine.add([this.banner, this.touchStart, this.newGame]);
+      this.engine.add([this.banner, this.touchStart, this.newGame, this.showCredits, this.credits]);
       if (this.state.saved)
-        this.engine.add([this.resume, this.highScore]);
+        this.engine.add([this.resume, this.savedGame]);
 
       if (this.ready)
-        setTimeout(this.showResume.bind(this), 100);
+        setTimeout(this.showButtons.bind(this), 100);
       else
-        this.listenTo(this.engine, "tap", this.onTouchToStart);
+        this.listenTo(this.engine, "tap", this.onTouchStart);
     },
     onDetach: function() {
       this.stopListening(this.engine);
-      this.engine.remove([this.banner, this.touchStart, this.newGame, this.highScore, this.resume]);
+      this.engine.remove([this.banner, this.touchStart, this.newGame, this.showCredits, this.credits, this.savedGame, this.resume]);
     },
-    spawnImg: Backbone.SpriteSheet.prototype.spawnImg,
-    onTouchToStart: function(e) {
+    onTouchStart: function(e) {
       // Animate some stuff
-      this.newGame.set({
-        targetX: -this.newGame.get("width") + this.newGame.textMetrics.width + this.newGame.get("textPadding")*2,
-        targetY: this.newGame.get("y")
-      });
-      if (this.state.saved) this.showResume();
       this.banner.set({
         targetX: this.banner.get("x"),
         targetY: 50
@@ -160,16 +210,51 @@
       });
       this.stopListening(this.engine);
       this.ready =  true;
+      this.showButtons();
     },
-    showResume: function() {
-      this.resume.set({
-        targetX: -this.resume.get("width") + this.resume.textMetrics.width + this.resume.get("textPadding")*2,
-        targetY: this.resume.get("y")
+    showButtons: function() {
+      this.newGame.set({
+        targetX: -this.newGame.get("width") + this.newGame.textMetrics.width + this.newGame.get("textPadding")*2,
+        targetY: this.newGame.get("y")
       });
-      this.highScore.set({
-        targetX: 720,
-        targetY: this.highScore.get("y")
+      this.showCredits.set({
+        targetX: -this.showCredits.get("width") + this.showCredits.textMetrics.width + this.showCredits.get("textPadding")*2,
+        targetY: this.showCredits.get("y")
       });
+      if (this.state.saved) {
+        this.resume.set({
+          targetX: -this.resume.get("width") + this.resume.textMetrics.width + this.resume.get("textPadding")*2,
+          targetY: this.resume.get("y")
+        });
+        this.savedGame.set({
+          targetX: 720,
+          targetY: this.savedGame.get("y")
+        });
+      }
+    },
+    hideButtons: function() {
+      this.newGame.set("targetX", -this.newGame.get("width"));
+      this.showCredits.set("targetX", -this.showCredits.get("width"));
+      this.resume.set("targetX", -this.resume.get("width"));
+      this.savedGame.set("targetX", 960);
+    },
+    showPanel: function(panel) {
+      this.panel = panel;
+      this.panel.set({
+        targetX: this.panel.get("x"),
+        targetY: 200
+      });
+      this.hideButtons();
+      this.listenTo(this.engine, "tap", this.hidePanel);
+    },
+    hidePanel: function() {
+      this.stopListening(this.engine);
+      this.panel.set({
+        targetX: this.panel.get("x"),
+        targetY: 720
+      });
+      this.panel = undefined;
+      this.showButtons();
     },
     update: function(dt) {
       return true;
